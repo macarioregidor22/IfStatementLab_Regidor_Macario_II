1c7a26920f3d93f778d75d8095af5080ea698246
macarioregidor22 <macarioregidor@g.cjc.edu.ph>
2026-09-20 13:00:13 +0800

    Add files via upload

---
+++ Age_Category_Checker/src/age_category_checker/Age_Category_Checker.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package age_category_checker;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Age_Category_Checker {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
